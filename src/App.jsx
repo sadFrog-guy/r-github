91ed950030bff91af16fdb13fc01876141de4e5e
sadFrog-guy <adikokey@mail.ru>
2024-10-16 17:29:12 +0600

    Init push

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function handleIncrement() {
+    setCount(prev => prev + 1)
+  }
+
+  function handleDecrement() {
+    setCount(prev => prev - 1)
+  }
+
+  return (
+    <div className="wrapper">
+      <h1 className="heading">
+        {count}
+      </h1> 
+
+      <div className="buttons">
+        <button 
+          className="button"
+          onClick={handleIncrement}
+        >
+          +
+        </button>
+
+        <button 
+          className="button"
+          onClick={handleDecrement}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
